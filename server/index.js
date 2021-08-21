facfa544b0beee56ff4667e8b37f483898e065db
Bihini Amarasinghe <bihiniamarasinghe98@gmail.com>
2021-08-21 19:45:45 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import studentRoutes from './routes/student.js';
+
+
+const app = express();
+
+app.use(bodyParser.json({limit:"20mb" ,extended:true}));
+app.use(bodyParser.urlencoded({limit:"20mb" ,extended:true}));
+
+app.use(cors());
+
+app.use('/students', studentRoutes);
+
+const CONNECTION_URL ='mongodb+srv://Bihi:Amara98@test1.ostua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+
+const PORT =process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{
+    useNewUrlParser:true, useUnifiedTopology:true
+}).then(()=> app.listen(PORT,()=>
+    console.log(`Connection is established and running on port :${PORT}`)
+)).catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify',false);
---
+++ server/models/student.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const studentSchema = mongoose.Schema({
+    regNo: Number,
+    studentName: String,
+    grade: String,
+    section: {
+        type: String,
+        default: 'A'
+    }
+
+});
+
+const student = mongoose.model('student', studentSchema);
+
+export default student;
